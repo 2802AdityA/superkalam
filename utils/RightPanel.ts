c31bf8c7036fd91fd7b8d3318319388b02c9bacf
Aditya Singla <adityasingla.2802@gmail.com>
2024-08-01 05:11:38 +0530

    data for right panel

---
+++ utils/RightPanel.ts
@@ -0,0 +1,64 @@
+import cat from "../public/cat.png";
+
+const rightPanel = [
+	{
+		title: "Recently Played",
+		content: [
+			{
+				cover: cat,
+				name: "Runaway",
+				artist: "Kanye West",
+				time: "5 min ago",
+			},
+			{
+				cover: cat,
+				name: "MY EYES",
+				artist: "Travis Scott",
+				time: "15 min ago",
+			},
+			{
+				cover: cat,
+				name: "MIDDLE CHILD",
+				artist: "J. Cole",
+				time: "18 min ago",
+			},
+			{
+				cover: cat,
+				name: "Sparks",
+				artist: "Coldplay",
+				time: "2 hr ago",
+			},
+		],
+	},
+	{
+		title: "My Playlist",
+		content: [
+			{
+				cover: cat,
+				name: "land of rising sun",
+				artist: "38 songs",
+				time: "2hr 43min",
+			},
+			{
+				cover: cat,
+				name: "burning memories",
+				artist: "21 songs",
+				time: "1hr 4min",
+			},
+			{
+				cover: cat,
+				name: "dream 127 U",
+				artist: "35 songs",
+				time: "1hr 56min",
+			},
+			{
+				cover: cat,
+				name: "underrated",
+				artist: "18 songs",
+				time: "51min",
+			},
+		],
+	},
+];
+
+export default rightPanel;
