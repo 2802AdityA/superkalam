fc2a34b6a5eaaf2f96f11c222d4a169b460b771e
Aditya Singla <adityasingla.2802@gmail.com>
2024-08-01 05:10:38 +0530

    correct types

--- components/Left/PanelColumn.tsx
+++ components/Left/PanelColumn.tsx
@@ -1,14 +1,15 @@
 "use client"
 
+import { LeftContent } from "@/interface"
 import { PanelRow } from "./PanelRow"
 
-export const PanelColumn = ({ title, content }: { title: string, content: any }) => {
+export const PanelColumn = ({ title, content }: { title: string, content: LeftContent[] }) => {
     return <div className="mt-4">
         <div className="text-xs text-gray-500 mt-4">
             {title}
         </div>
         <div className="mt-3 mb-6">
-            {content.map((con: any) => {
+            {content.map((con: LeftContent) => {
                 return <PanelRow key={con.title} content={con} />
             })}
         </div>
--- components/Left/PanelRow.tsx
+++ components/Left/PanelRow.tsx
@@ -1,8 +1,9 @@
 "use client"
 
+import { LeftContent } from "@/interface"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
-export const PanelRow = ({ content }: { content: any }) => {
+export const PanelRow = ({ content }: { content: LeftContent }) => {
     return <div className="flex justify-start items-center mb-3">
         <FontAwesomeIcon className={`h-5 ${(content.selected === true) ? 'text-blue-600' : ' text-gray-400'}`} icon={content.icon} />
         <div className={`ml-4 ${(content.selected === true) ? 'text-blue-600' : ' text-gray-400'}`}>
