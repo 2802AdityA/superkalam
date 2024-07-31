24e40b2ebf8c3469e6ca6c5dc86d132dcf801197
Aditya Singla <adityasingla.2802@gmail.com>
2024-07-31 20:12:14 +0530

    remove unused code

--- components/Left/PanelColumn.tsx
+++ components/Left/PanelColumn.tsx
@@ -9,19 +9,6 @@ export const PanelColumn = ({ title, content }: { title: string, content: any })
             {content.map((con: any) => {
                 return <PanelRow key={con.title} content={con} />
             })}
-            {/* <div className="flex items-center">
-                <FontAwesomeIcon className="h-5" icon={faHouse} />
-                <div className="ml-4">
-                    Home
-                </div>
-            </div> */}
-            {/* <div>
-                <FontAwesomeIcon icon={faMagnifyingGlass} />
-                Discover
-            </div>
-            <div>
-                Collections
-            </div> */}
         </div>
     </div>
 }
