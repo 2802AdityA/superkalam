8ff411a2de33870419265ca5ac6fd96e3345b6ad
Aditya Singla <adityasingla.2802@gmail.com>
2024-08-01 01:56:57 +0530

    small style change

--- components/Left/PanelColumn.tsx
+++ components/Left/PanelColumn.tsx
@@ -7,7 +7,7 @@ export const PanelColumn = ({ title, content }: { title: string, content: any })
         <div className="text-xs text-gray-500 mt-4">
             {title}
         </div>
-        <div className="mt-4 mb-6">
+        <div className="mt-3 mb-6">
             {content.map((con: any) => {
                 return <PanelRow key={con.title} content={con} />
             })}
