c4e69257fc8e14df4acc74435cb976f62cc0fee5
Aditya Singla <adityasingla.2802@gmail.com>
2024-08-01 23:13:32 +0530

    Add player component

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
                     <Right />
                 </div>
             </div>
-            <div className="bg-rightC p-4 border-0 border-t-2 h-20 border-[#1A1B1C] fixed bottom-0 w-full">
+            <div className="bg-rightC px-8 py-4 border-0 border-t-2 h-30 border-[#1A1B1C] fixed bottom-0 w-full">
                 <Player />
             </div>
         </div>
--- components/Player.tsx
+++ components/Player.tsx
@@ -1,5 +1,67 @@
 "use client"
+import { CirclePause, Heart, Mic, MonitorSpeaker, Repeat, Share2, Shuffle, SkipBack, SkipForward, SquarePlus, Volume2 } from 'lucide-react';
 
 export const Player = () => {
-    return <div className="text-white ">player</div>
+    return (
+        <div className="text-white flex flex-row justify-between items-center">
+            <div className="flex flex-row justify-between items-center space-x-4">
+                <div className="flex flex-col">
+                    <div>
+                        Runaway
+                    </div>
+                    <div>
+                        Kanye West
+                    </div>
+                </div>
+                <div className="flex space-x-2">
+                    <div>
+                        <Heart />
+                    </div>
+                    <div>
+                        <SquarePlus />
+                    </div>
+                </div>
+
+            </div>
+            <div className="flex flex-col justify-center items-center flex-grow space-y-4">
+                <div className="flex w-full flex-row justify-center items-center">
+                    <div>
+                        <Shuffle />
+                    </div>
+                    <div>
+                        <SkipBack />
+                    </div>
+                    <div>
+                        <CirclePause />
+                    </div>
+                    <div>
+                        <SkipForward />
+                    </div>
+                    <div>
+                        <Repeat />
+                    </div>
+                </div>
+                <div className='flex w-full justify-center items-center'>
+                    <input className='w-1/2' type="range" min="1" max="100" value="40" />
+                </div>
+            </div>
+            <div className="flex items-center">
+                <div>
+                    <Volume2 />
+                </div>
+                <div className='w-16'>
+                    <input className='w-16' type="range" min="1" max="100" value="40" />
+                </div>
+                <div>
+                    <Mic />
+                </div>
+                <div>
+                    <MonitorSpeaker />
+                </div>
+                <div>
+                    <Share2 />
+                </div>
+            </div>
+        </div>
+    )
 }
