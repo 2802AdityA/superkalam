269e9633079a3d56d069471477237dac196fd50f
Aditya Singla <adityasingla.2802@gmail.com>
2024-08-02 03:40:38 +0530

    increase size of player icons

--- components/Player/MusicPlayer.tsx
+++ components/Player/MusicPlayer.tsx
@@ -1,25 +1,26 @@
 "use client"
 
 import { CirclePause, Repeat, Shuffle, SkipBack, SkipForward } from "lucide-react"
+import { PlayerButton } from './PlayerButton';
 
 export const MusicPlayer = () => {
     return <div className="flex flex-col justify-center items-center flex-grow space-y-4">
         <div className="flex w-full flex-row justify-center items-center space-x-8">
-            <div className="text-gray-400">
-                <Shuffle />
-            </div>
-            <div className="text-gray-400">
-                <SkipBack />
-            </div>
-            <div className="text-gray-400">
-                <CirclePause />
-            </div>
-            <div className="text-gray-400">
-                <SkipForward />
-            </div>
-            <div className="text-gray-400">
-                <Repeat />
-            </div>
+            <PlayerButton>
+                <Shuffle size={27} />
+            </PlayerButton>
+            <PlayerButton>
+                <SkipBack size={27} />
+            </PlayerButton>
+            <PlayerButton>
+                <CirclePause size={27} />
+            </PlayerButton>
+            <PlayerButton>
+                <SkipForward size={27} />
+            </PlayerButton>
+            <PlayerButton>
+                <Repeat size={27} />
+            </PlayerButton>
         </div>
         <div className='flex w-full  justify-center items-center'>
             <input className='w-1/2 h-1' type="range" min="1" max="100" value="40" />
