925edd3675fee827970c782cae3bc56c43212fc4
Aditya Singla <adityasingla.2802@gmail.com>
2024-08-01 23:41:38 +0530

    Add use client

--- components/Center/TopBar.tsx
+++ components/Center/TopBar.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { faAngleLeft, faAngleRight, faEllipsis, faSearch } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
