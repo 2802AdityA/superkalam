80e3e0fae73141e1dd9bd8e45ba6c7abc70de6df
Aditya Singla <adityasingla.2802@gmail.com>
2024-08-01 20:19:28 +0530

    Data for center component

---
+++ utils/CenterPanel.ts
@@ -0,0 +1,93 @@
+import krsna from "../public/TimeWillTell.jpeg";
+import sm from "../public/Nayaab.jpeg";
+import anjum from "../public/OpenLetter.jpeg";
+import astro from "../public/ASTROWORLD.jpeg";
+import cat from "../public/cat.png";
+import off from "../public/TheOffSeason.jpeg";
+import kanye from "../public/Graduation.jpeg";
+import damn from "../public/DAMN.jpeg";
+import gotye from "../public/SomebodyThatIUsedToKnow.jpeg";
+import em from "../public/TheMarshallMathersLP.jpeg";
+import sab from "../public/SabChahiye.jpeg";
+import tamasha from "../public/Tamasha.jpeg";
+import rockstar from "../public/Rockstar.jpeg";
+
+const centerPanel = [
+	{
+		title: "Hello, aditya",
+		content: [
+			{
+				cover: krsna,
+				title: "Time Will Tell",
+				sub: "KR$NA",
+			},
+			{
+				cover: sm,
+				title: "NAYAAB",
+				sub: "Seedhe Maut",
+			},
+			{
+				cover: anjum,
+				title: "Open Letter",
+				sub: "Talha Anjum",
+			},
+			{
+				cover: astro,
+				title: "ASTROWORLD",
+				sub: "Travis Scott",
+			},
+		],
+	},
+	{
+		title: "New releases for you",
+		content: [
+			{
+				cover: off,
+				title: "The Off-Season",
+				sub: "J. Cole",
+			},
+			{
+				cover: kanye,
+				title: "Graduation",
+				sub: "Kanye West",
+			},
+			{
+				cover: damn,
+				title: "DAMN.",
+				sub: "Kendrick Lamar",
+			},
+			{
+				cover: gotye,
+				title: "Somebody That I Used To Know",
+				sub: "Gotye ft. Kimbra",
+			},
+		],
+	},
+	{
+		title: "Jump Back In",
+		content: [
+			{
+				cover: em,
+				title: "The Marshall Marthers LP",
+				sub: "Eminem",
+			},
+			{
+				cover: sab,
+				title: "Sab Chahiye",
+				sub: "Rawal, Bharg",
+			},
+			{
+				cover: tamasha,
+				title: "Tamasha",
+				sub: "A.R. Rahman",
+			},
+			{
+				cover: rockstar,
+				title: "Rockstar",
+				sub: "A.R. Rahman",
+			},
+		],
+	},
+];
+
+export default centerPanel;
