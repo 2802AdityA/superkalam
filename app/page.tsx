bfced406e57b389a156b70ae66882fdf4fde7051
Aditya Singla <adityasingla.2802@gmail.com>
2024-08-01 20:16:58 +0530

    UI fix - height of center

--- app/page.tsx
+++ app/page.tsx
@@ -7,19 +7,20 @@ export default function Home() {
 
 
     return (
-        <div className="flex-col h-screen overflow-hidden">
-            <div className="flex flex-grow ">
-                <div className="w-1/6 min-w-1/6 bg-leftC overflow-y-auto h-full min-h-screen">
+        <div className="flex flex-col h-screen overflow-hidden">
+            <div className="flex overflow-hidden flex-grow pb-16">
+                <div className="w-0 lg:w-1/6 lg:min-w-1/6 bg-leftC overflow-y-auto min-h-screen">
                     <Left />
                 </div>
-                <div className="flex-1 bg-centerC overflow-y-auto h-full min-h-screen ">
+                <div
+                    className="flex-1 pb-16 bg-centerC overflow-y-scroll scrollbar-thin scrollbar-thumb-transparent scrollbar-track-transparent ">
                     <Center />
                 </div>
-                <div className="w-1/5 min-w-[400px] bg-rightC overflow-y-auto h-full min-h-screen ">
+                <div className="w-0 lg:w-1/5 lg:min-w-[400px] bg-rightC overflow-y-auto h-full min-h-screen">
                     <Right />
                 </div>
             </div>
-            <div className="bg-rightC p-4 border-0 border-t-2 border-[#1A1B1C] fixed bottom-0 w-full">
+            <div className="bg-rightC p-4 border-0 border-t-2 h-20 border-[#1A1B1C] fixed bottom-0 w-full">
                 <Player />
             </div>
         </div>
