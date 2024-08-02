f6d9113bc1d7f3c7ac82d209546056b16d252596
Aditya Singla <adityasingla.2802@gmail.com>
2024-08-02 18:47:52 +0530

    Player width fix

--- components/Player/Player.tsx
+++ components/Player/Player.tsx
@@ -7,13 +7,13 @@ import { Sound } from './Sound';
 export const Player = () => {
     return (
         <div className="text-white flex flex-row justify-between items-center">
-            <div className='w-2/12'>
+            <div className='w-1/4'>
                 <Song />
             </div>
             <div className='w-full'>
                 <MusicPlayer />
             </div>
-            <div className='hidden xl:block xl:w-2/12'>
+            <div className='hidden xl:block xl:w-1/4'>
                 <Sound />
             </div>
         </div>
