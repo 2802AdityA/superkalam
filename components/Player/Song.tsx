7f3ea6aaffed78a0a58925745379f25b52934f95
Aditya Singla <adityasingla.2802@gmail.com>
2024-08-02 18:43:23 +0530

    Text size changes for smaller screen

--- components/Player/Song.tsx
+++ components/Player/Song.tsx
@@ -5,11 +5,11 @@ import { PlayerButton } from './PlayerButton';
 
 export const Song = () => {
     return <div className="flex flex-row justify-between items-center space-x-8">
-        <div className="flex flex-col">
+        <div className="flex flex-col flex-grow">
             <div className="font-semibold ">
                 Runaway
             </div>
-            <div className="font-light text-gray-400 hover:underline hover:text-gray-200">
+            <div className="font-light text-xs sm:text-sm  text-gray-400 hover:underline hover:text-gray-200">
                 Kanye West
             </div>
         </div>
