6967f1d993b6b5fe594b4d692c3943b90449c301
Aditya Singla <adityasingla.2802@gmail.com>
2024-08-02 18:43:09 +0530

    Changes for smaller screen

--- components/Player/PlayerButton.tsx
+++ components/Player/PlayerButton.tsx
@@ -1,4 +1,4 @@
 import { ReactNode } from 'react';
 export const PlayerButton = ({ children }: { children: ReactNode }) => {
-    return <button className="text-gray-500 hover:text-white transition duration-500">{children}</button>
+    return <button className="hidden lg:block text-gray-500 hover:text-white transition duration-500">{children}</button>
 }
