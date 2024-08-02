10e91f8945008a5c851a88076a41bb263e9b8822
Aditya Singla <adityasingla.2802@gmail.com>
2024-08-02 18:41:50 +0530

    Remove some buttons for small screen

--- components/Player/MusicPlayer.tsx
+++ components/Player/MusicPlayer.tsx
@@ -1,29 +1,41 @@
 "use client"
 
 import { CirclePause, Repeat, Shuffle, SkipBack, SkipForward } from "lucide-react"
-import { PlayerButton } from './PlayerButton';
+import { ReactNode } from "react"
 
 export const MusicPlayer = () => {
     return <div className="flex flex-col justify-center items-center flex-grow space-y-4">
         <div className="flex w-full flex-row justify-center items-center space-x-8">
-            <PlayerButton>
-                <Shuffle size={27} />
-            </PlayerButton>
+            <div className="hidden sm:block">
+                <PlayerButton>
+                    <Shuffle size={27} />
+                </PlayerButton>
+            </div>
+            {/* <div className="hidden sm:block"> */}
             <PlayerButton>
                 <SkipBack size={27} />
             </PlayerButton>
+            {/* </div> */}
             <PlayerButton>
                 <CirclePause size={27} />
             </PlayerButton>
+            {/* <div className="hidden sm:block"> */}
             <PlayerButton>
                 <SkipForward size={27} />
             </PlayerButton>
-            <PlayerButton>
-                <Repeat size={27} />
-            </PlayerButton>
+            {/* </div> */}
+            <div className="hidden sm:block">
+                <PlayerButton>
+                    <Repeat size={27} />
+                </PlayerButton>
+            </div>
         </div>
         <div className='flex w-full  justify-center items-center'>
             <input className='w-1/2 h-1' type="range" min="1" max="100" value="40" />
         </div>
     </div>
+}
+
+const PlayerButton = ({ children }: { children: ReactNode }) => {
+    return <button className=" text-gray-500 hover:text-white transition duration-500">{children}</button>
 }
