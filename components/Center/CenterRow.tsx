febc86bf3659e8515c94a020256619fc72b11373
Aditya Singla <adityasingla.2802@gmail.com>
2024-08-02 18:03:50 +0530

    Center heading fix

--- components/Center/CenterRow.tsx
+++ components/Center/CenterRow.tsx
@@ -5,15 +5,15 @@ import { CenterContent, CenterPanelInterface } from "@/interface";
 
 export const CenterRow = ({ row }: { row: CenterPanelInterface }) => {
     return (<div className="mt-8 flex flex-col ">
-        <div className="flex justify-between items-center mb-8">
-            <div className="font-extrabold text-3xl hover:underline">
+        <div className="flex justify-between items-center my-8 ">
+            <div className="font-extrabold text-2xl lg:text-2xl xl:text-3xl hover:underline">
                 {row.title}
             </div>
             <div>
                 <button className="text-gray-400 hover:text-white transition duration-500 font-semibold">See all</button>
             </div>
         </div>
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-[4%]">
+        <div className="grid grid-cols-2  lg:grid-cols-4 gap-[4%]">
             {
                 row.content.map((playlist: CenterContent) => {
                     return <Playlist key={playlist.title} playlist={playlist} />
